4675c0b4d60eeb324aa64410cebdf9fb5800c34c
Dev Agrawal <devagrawal09@gmail.com>
2026-05-24 21:26:46 -0500

    del

--- src/features/events.ts
+++
@@ -1,5 +0,0 @@
-import type { HarlanEvent } from './harlan/events'
-import type { ThreadplaneEvent } from './threadplane/events'
-import type { Event as TodoEvent } from './todos/events'
-
-export type Event = TodoEvent | HarlanEvent | ThreadplaneEvent
