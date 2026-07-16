c578348e2d5f5baffa4598019024fc567586904b
Dev Agrawal <devagrawal09@gmail.com>
2026-07-16 08:23:12 -0500

    chore: remove envelope migration codemod

--- codemods/specter-envelope-api/tests/edge-double-quoted-aliased-factory/input.tsx
+++
@@ -1,7 +0,0 @@
-import { defineSpecterClient as makeTransport } from "@specter-ts/core/client"
-
-const customClient = makeTransport<Config>("/api")
-
-export async function load() {
-  return customClient.todosQuery({ status: "all" })
-}
